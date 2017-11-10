a19c885eb82f85a300b2190abfc4f33d7ea8eef3
cash <cash@cashwu.com>
2017-11-10 09:50:33 +0800

    Fifteen Love

--- Kata20171110_TennisGame/TennisGameTests.cs
+++ Kata20171110_TennisGame/TennisGameTests.cs
@@ -12,13 +12,33 @@ namespace Kata20171110_TennisGame
             string score = tennisGame.Score();
             Assert.AreEqual("Love All", score);
         }
+
+        [TestMethod]
+        public void FifteenLove()
+        {
+            TennisGame tennisGame = new TennisGame();
+            tennisGame.PlayerOneScore();
+            string score = tennisGame.Score();
+            Assert.AreEqual("Fifteen Love", score);
+        }
     }
 
     public class TennisGame
     {
+        private int playerOneScore;
+
         public string Score()
         {
+            if (playerOneScore != 0)
+            {
+                return "Fifteen Love";
+            }
             return "Love All";
+        }
+
+        public void PlayerOneScore()
+        {
+            this.playerOneScore++;
         }
     }
 }
